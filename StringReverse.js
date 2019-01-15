b198d54bb6ec1df967a7e1140138f2eb5554f5bf
tchoutdara <monkeymilitia444@gmail.com>
2019-01-15 13:11:38 -0700

    add comments to string reverse

--- StringReverse.js
+++ StringReverse.js
@@ -1,6 +1,6 @@
 function strReverse(str){
+    //Take a string and split each letter into an array, then reverse the order and rejoin them back into a string
     var reversed = str.split("").reverse().join("")
-
     return reversed
 }
 
